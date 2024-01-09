6b759f4db7b09e2b6f021badf721114bf0e23b57
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2024-01-09 19:34:51 +0000

    Implement stablehlo dot general -> xnnpack batch matmul conversion
    This commit fully implements the conversion from
    `stablehlo.dot_general` to `xnnpack.batch_matrix_multiply`. Because
    `xnnpack.batch_matrix_multiply` does not support broadcasting, the
    pattern broadcasts the input tensors using stablehlo ops.

--- samples/compiler_plugins/xnnpack/src/xnnpack/Conversion/StablehloToXnnpack.cpp
+++ samples/compiler_plugins/xnnpack/src/xnnpack/Conversion/StablehloToXnnpack.cpp
@@ -4,6 +4,9 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <cstdint>
+
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "stablehlo/dialect/StablehloOps.h"
 #include "xnnpack/Conversion/Passes.h"
@@ -24,8 +27,52 @@ class ConvertDotGeneralOp
       ConversionPatternRewriter &rewriter) const override {
     Value lhs = adaptor.getLhs();
     Value rhs = adaptor.getRhs();
+    auto lhsType = lhs.getType().cast<RankedTensorType>();
+    auto rhsType = rhs.getType().cast<RankedTensorType>();
+
+    auto dotNumbers = op.getDotDimensionNumbers();
+    ArrayRef<int64_t> lhsContractingDims =
+        dotNumbers.getLhsContractingDimensions();
+    ArrayRef<int64_t> rhsContractingDims =
+        dotNumbers.getRhsContractingDimensions();
+    if (lhsContractingDims.size() != 1 || rhsContractingDims.size() != 1) {
+      return rewriter.notifyMatchFailure(
+          op, "expected at most one contracting dimension");
+    }
+    int64_t lhsContractingDim = lhsContractingDims.front();
+    int64_t rhsContractingDim = rhsContractingDims.front();
+    int64_t lhsBatchDimsCount = lhsType.getRank() - 2;
+    int64_t rhsBatchDimsCount = rhsType.getRank() - 2;
+    if (lhsContractingDim - lhsBatchDimsCount != 1 ||
+        rhsContractingDim - rhsBatchDimsCount != 0) {
+      return rewriter.notifyMatchFailure(op,
+                                         "not a batch matrix multiplication");
+    }
+
+    auto broadcastBatchDims = [&op, &rewriter](Value input,
+                                               int64_t batchDimCount) -> Value {
+      auto inputType = input.getType().cast<RankedTensorType>();
+      auto resultType = op.getType().cast<RankedTensorType>();
+      int64_t resultBatchDimCount = resultType.getRank() - 2;
+      ArrayRef<int64_t> inputShape(inputType.getShape());
+      SmallVector<int64_t> broadcastShape(resultType.getShape());
+      broadcastShape[resultBatchDimCount] = inputShape[batchDimCount];
+      broadcastShape[resultBatchDimCount + 1] = inputShape[batchDimCount + 1];
+      Type broadcastType =
+          RankedTensorType::get(broadcastShape, inputType.getElementType());
+
+      int64_t inputOutputDimDiff = resultBatchDimCount - batchDimCount;
+      DenseIntElementsAttr broadcastDims =
+          rewriter.getI64TensorAttr(llvm::to_vector(llvm::seq(
+              inputOutputDimDiff, inputType.getRank() + inputOutputDimDiff)));
+      return rewriter.create<mlir::stablehlo::BroadcastInDimOp>(
+          op.getLoc(), broadcastType, input, broadcastDims);
+    };
+
+    Value lhsBroadcast = broadcastBatchDims(lhs, lhsBatchDimsCount);
+    Value rhsBroadcast = broadcastBatchDims(rhs, rhsBatchDimsCount);
     rewriter.replaceOpWithNewOp<Xnnpack::BatchMatrixMultiplyOp>(
-        op, op.getType(), lhs, rhs);
+        op, op.getType(), lhsBroadcast, rhsBroadcast);
     return success();
   }
 };
@@ -42,7 +89,8 @@ class ConvertStablehloToXnnpackPass
   void runOnOperation() override {
     MLIRContext *context = &getContext();
     ConversionTarget target(*context);
-    target.addLegalDialect<IREE::Xnnpack::XnnpackDialect>();
+    target.addLegalDialect<IREE::Xnnpack::XnnpackDialect,
+                           mlir::stablehlo::StablehloDialect>();
     TypeConverter typeConverter;
     typeConverter.addConversion([](Type type) { return type; });
     RewritePatternSet patterns(context);
--- samples/compiler_plugins/xnnpack/test/stablehlo-to-xnnpack.mlir
+++ samples/compiler_plugins/xnnpack/test/stablehlo-to-xnnpack.mlir
@@ -1,7 +1,11 @@
 // RUN: iree-opt --iree-plugin=xnnpack --iree-print-plugin-info --pass-pipeline='builtin.module(iree-stablehlo-to-xnnpack)' %s | FileCheck %s
 
 // CHECK-LABEL:   func.func @dot_general(
-// CHECK:           %{{.*}} = xnnpack.batch_matrix_multiply
+// CHECK-SAME:                           %[[LHS:.*]]: tensor<1x100x200xi8>,
+// CHECK-SAME:                           %[[RHS:.*]]: tensor<200x300xi8>) -> tensor<1x100x300xi32> {
+// CHECK:           %[[LHS_BROADCAST:.*]] = stablehlo.broadcast_in_dim %[[LHS]], dims = [0, 1, 2] : (tensor<1x100x200xi8>) -> tensor<1x100x200xi8>
+// CHECK:           %[[RHS_BROADCAST:.*]] = stablehlo.broadcast_in_dim %[[RHS]], dims = [1, 2] : (tensor<200x300xi8>) -> tensor<1x200x300xi8>
+// CHECK:           %{{.*}} = xnnpack.batch_matrix_multiply %[[LHS_BROADCAST]], %[[RHS_BROADCAST]] : (tensor<1x100x200xi8>, tensor<1x200x300xi8>) -> tensor<1x100x300xi32>
 func.func @dot_general(%a : tensor<1x100x200xi8>, %b : tensor<200x300xi8>) -> tensor<1x100x300xi32> {
   %out = stablehlo.dot_general %a, %b, contracting_dims = [2] x [0], precision = [DEFAULT, DEFAULT] : (tensor<1x100x200xi8>, tensor<200x300xi8>) -> tensor<1x100x300xi32>
   func.return %out : tensor<1x100x300xi32>
