976a2f697f258d991025c14f63ba756502d3d8f8
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2024-01-23 19:08:02 +0000

    [NFC] Remove mentions of `xnnpack_sample`

--- samples/compiler_plugins/iree_compiler_plugin.cmake
+++ samples/compiler_plugins/iree_compiler_plugin.cmake
@@ -6,4 +6,4 @@
 
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/example samples/example)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/simple_io_sample samples/simple_io_sample)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/xnnpack samples/xnnpack_sample)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/xnnpack samples/xnnpack)
