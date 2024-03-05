d6cbd43f47996cd42e78c28613aa93b16da777db
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2024-03-05 19:40:28 +0000

    Make constant check more specific to fully_connected case

--- samples/compiler_plugins/xnnpack/src/xnnpack/Transforms/SetIdAttributes.cpp
+++ samples/compiler_plugins/xnnpack/src/xnnpack/Transforms/SetIdAttributes.cpp
@@ -15,26 +15,19 @@ namespace mlir::iree_compiler::IREE::Xnnpack {
 #include "xnnpack/Transforms/Passes.h.inc"
 
 namespace {
-// Checks if `val` is a constant by going up the use-def tree to see if the
-// leaves of the tree are `stablehlo` constants.
+// Checks if the kernel of `fc` is a constant.
 //
 // Note: a returned value of false means that this function was unable to
 // determine if the value is constant or not.
-static bool isConstant(Value val) {
-  SmallVector<Value> stack{val};
-  SmallVector<Operation *> zeroOperandOps;
-  while (!stack.empty()) {
-    Value currVal = stack.pop_back_val();
-    if (auto defOp = currVal.getDefiningOp()) {
-      if (defOp->getNumOperands() == 0) zeroOperandOps.push_back(defOp);
-      stack.append(SmallVector<Value>(defOp->getOperands()));
-    } else {
-      return false;
-    }
-  }
-  return llvm::all_of(zeroOperandOps, [](Operation *op) {
-    return isa<stablehlo::ConstantOp>(op);
-  });
+static bool kernelIsConstant(FullyConnectedNcQd8F32Qc4wOp fc) {
+  Value kernel = fc.getKernel();
+  // The current implementation of the fully connected XNNPACK op requires that
+  // we transform the kernel from signed int space to unsigned int space. This
+  // is currently done through an XOR operation, hence the special case handling
+  // here.
+  if (auto xorOp = kernel.getDefiningOp<stablehlo::XorOp>())
+    kernel = xorOp.getLhs();
+  return kernel.getDefiningOp<stablehlo::ConstantOp>();
 }
 
 class SetIdAttributesPass
@@ -46,12 +39,12 @@ class SetIdAttributesPass
     auto m = getOperation();
     DenseMap<Value, int64_t> kernelIds;
     auto result =
-        m.walk([&](Xnnpack::FullyConnectedNcQd8F32Qc4wOp fc) -> WalkResult {
-          Value kernel = fc.getKernel();
-          if (!isConstant(kernel))
+        m.walk([&](FullyConnectedNcQd8F32Qc4wOp fc) -> WalkResult {
+          if (!kernelIsConstant(fc))
             return fc.emitError(
                 "unimplemented: found kernel that is not guaranteed to be "
                 "constant");
+          Value kernel = fc.getKernel();
           if (!kernelIds.contains(kernel)) kernelIds[kernel] = kernelIds.size();
           auto kernelIdAttr = IntegerAttr::get(IndexType::get(m.getContext()),
                                                kernelIds[kernel]);
